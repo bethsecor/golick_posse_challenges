96557441c1126e6575a0c9fe618016c5d010d855
Sal Espinosa <sespinos@gmail.com>
2016-02-18 06:22:34 -0700

    Update with querying methods

--- people_database/people_database.rb
+++ people_database/people_database.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class People
+  attr_reader :db
+
   def initialize(people = [])
     @db = people
   end
@@ -16,4 +18,39 @@ class People
     end
     @db.delete(person_to_delete)
   end
+
+  def remove_by_state(state)
+    people_to_delete = @db.select{ |person| person.state == state}
+    people_to_delete.each {|person| @db.delete(person)}
+  end
+
+  def email_addresses
+    emails = @db.map{|person| person.email}
+    puts emails.join(", ")
+  end
+
+  def state_count(state)
+    from_state = @db.select{|person| person.state == state}
+    from_state.count
+  end
+end
+
+if __FILE__ == $0
+  pdb = People.new
+  pdb.add(first_name: "Tess", last_name: "Griffin", email: "tess@turing.io", state: "GA")
+  pdb.add(first_name: "Sal", last_name: "Espinosa", email: "sal@sal.com", state: "CO")
+  pdb.add(first_name: "Heidi", last_name: "Hoopes", email: "heidi@heidi.com", state: "CO")
+  pdb.add(first_name: "July", last_name: "Tran", email: "july@july.com", state: "CO")
+  pdb.add(first_name: "Beth", last_name: "Secor", email: "beth@beth.com", state: "TX")
+
+  pdb.email_addresses
+  pdb.remove_person_by_email("sal@sal.com")
+  pdb.email_addresses
+  puts pdb.state_count("CO")
+  puts pdb.state_count("GA")
+  puts pdb.state_count("TX")
+  pdb.remove_by_state("CO")
+  pdb.email_addresses
+  puts pdb.state_count("CO")
+
 end
