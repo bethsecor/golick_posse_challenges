feac5617811e88fb84f14cccb33d080b7b3b03dd
hhoopes <heidih@gmail.com>
2016-02-18 09:02:48 -0700

    Add tests

--- people_database/people_database.rb
+++ people_database/people_database.rb
@@ -1,4 +1,4 @@
-require_relative 'person'
+	require_relative 'person'
 
 class People
   attr_reader :db
---
+++ people_database/test/people_database_test.rb
@@ -0,0 +1,7 @@
+require_relative '../people_database'
+require 'minitest'
+
+class PeopleDatabase < Minitest::Test
+  def test_
+
+end
