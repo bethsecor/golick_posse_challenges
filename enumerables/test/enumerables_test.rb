60d5481c1800f2b88ab76a7e31187722fb5cf828
hhoopes <heidih@gmail.com>
2016-02-25 09:01:23 -0700

    Finish required methods, including tests with super awesome long words

--- enumerables/test/enumerables_test.rb
+++ enumerables/test/enumerables_test.rb
@@ -8,7 +8,7 @@ class EnumerablesTest < Minitest::Test  # => Minitest::Test
     odd_array = [1,5,7,9,15,21]
     assert_equal [2,6,8,10,12], even_array.select{|num| num % 2 == 0 }
     assert_equal [], odd_array.select{|num| num % 2 == 0}
-  end                                                # => :test_any_returns_true_or_false_if_any_have_that_condition
+  end                                                                   # => :test_select_returns_values_for_any_that_have_that_condition
 
   def test_any_returns_true_or_false_if_any_have_that_condition
     short_words = ["a", "be", "dog"]
@@ -17,16 +17,22 @@ class EnumerablesTest < Minitest::Test  # => Minitest::Test
     odd_array = [1,3,5,21]
 
     refute odd_array.any?{|num| num.even?}
-  end
+  end                                       # => :test_any_returns_true_or_false_if_any_have_that_condition
 
   def test_map_changes_values_in_array_by_block_argument
     array = [0,2,4,6]
 
     assert_equal [0,4,8,12], array.map {|value| value * 2}
-  end
+  end                                                       # => :test_map_changes_values_in_array_by_block_argument
 
+  def test_reject_excludes_values_that_dont_match_the_block
+    long_words = ["elephant", "pteradactyl", "floccinaucinihilipilification"]
 
-end  # => :test_any_returns_true_or_false_if_any_have_that_condition
+    assert_equal ["elephant"], long_words.reject{|word| word.length > 8 }
+    assert_equal long_words, long_words.reject{|word| word.length > "floccinaucinihilipilification".length}
+
+  end  # => :test_reject_excludes_values_that_dont_match_the_block
+end    # => :test_reject_excludes_values_that_dont_match_the_block
 
   def any?(array, &block)
     flag = false
@@ -34,19 +40,25 @@ end  # => :test_any_returns_true_or_false_if_any_have_that_condition
       flag = true if array.include? block.call(value)
     end
     flag
-  end                               # => :our_any?
+  end                                                  # => :any?
 
   def select(array, &block)
     new_array = []
     array.each {|value| new_array << value if block.call(value)}
     new_array
-  end
+  end                                                             # => :select
 
   def map(array, &block)
-    new_array = []                  # => []
-    array.each do |num|             # => [1, 2, 3]
-      new_array << block.call(num)  # => [2], [2, 4], [2, 4, 6]
-    end                             # => [1, 2, 3]
-    new_array                       # => [2, 4, 6]
-  end
-                                                  # => :select
+    new_array = []
+    array.each do |value|
+      new_array << block.call(value)
+    end
+    new_array
+  end                                 # => :map
+
+  def reject(array, &block)
+    new_array = []
+    array.each do | value |
+      new_array << value unless block.call(value)
+    end
+  end                                              # => :reject
